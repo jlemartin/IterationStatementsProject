292b42c975f30c0d3fb0ff8b39d16dedd0924f37
John Martin <jlemartin7@gmail.com>
2020-10-27 17:22:48 -0500

    completed exercise

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -36,14 +36,9 @@ namespace IterationStatements
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
 
-            // Create a for loop
-            // in your initializer set the value of i to 199
-            // in your conditional, as long as i is less than or equal to the amount of items in "numbers" (numbers.Count)
-            // and as long as i is greater than or equal to 0
-            // Decrement i by 1
             for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
-                // Write to the console "numbers" at index i
+                Console.WriteLine($"{numbers[i]}");
             }
         }
     }
